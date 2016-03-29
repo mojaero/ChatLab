58c2c46c91305b74b8b8d11aa4a1946e77d99053
mojaero <mo.malie@hotmail.com>
2016-03-29 21:51:12 +0200

    Initial project setup

---
+++ ChatLab/src/ChatLab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatLab
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+        }
+    }
+}
